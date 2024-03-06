73dd2d2c7842196a2bbe2a32f4d883d9027a95e8
Lakshmi Sravani <137598500+DataWhizML@users.noreply.github.com>
2024-03-06 14:20:16 +0530

    Add files via upload

---
+++ Youtube_audio.py
@@ -0,0 +1,32 @@
+from pytube import YouTube
+from moviepy.editor import *
+import os
+
+def download_youtube_audio(video_url, output_path , i ):
+    try:
+        yt = YouTube(video_url)
+        stream = yt.streams.filter(only_audio=True).first()
+        
+        audio_path = os.path.join(output_path, f"audio{i}.mp3")
+        print(audio_path)
+        if os.path.exists(audio_path):
+            os.remove(audio_path)
+            print("Existing file deleted.")
+
+        stream.download(output_path=output_path)
+        
+        video_clip = stream.default_filename
+        audio_path_temp = video_clip.replace(".mp4", ".mp3")
+        clip = AudioFileClip(video_clip)
+        clip.write_audiofile(audio_path)
+
+        os.remove(video_clip)
+
+        print("Audio downloaded successfully as MP3!")
+    except Exception as e:
+        print("Error:", str(e))
+
+if __name__ == "__main__":
+    video_url = str(input("Enter the YouTube video URL: "))
+    output_path = "."  # Use the current directory
+    download_youtube_audio(video_url, output_path)
